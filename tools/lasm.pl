5681b341e995e7406fede3a908e426c19ba25ef1
Brock Wilcox <awwaiid@thelackthereof.org>
2014-07-25 13:50:40 -0400

    Add label assembler

---
+++ tools/lasm.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use strict;
+
+my @proggie;
+my %label;
+
+while(my $line = <>) {
+  chomp $line;
+  $line =~ s/^\s+//;
+  $line =~ s/\s*;.*$//;
+  if($line =~ /^(\w+):$/) {
+    $label{$1} = scalar @proggie;
+  } else {
+    push @proggie, $line;
+  }
+}
+
+@proggie = map {
+  my $line = $_;
+  foreach my $label (keys %label) {
+    my $linenum = $label{$label};
+    $line =~ s/$label/$linenum/g;
+  }
+  $line
+} @proggie;
+
+print join("\n", @proggie);
+print "\n";
+
